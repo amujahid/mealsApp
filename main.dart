c42c533600a427e0b89fcefffaed540b736a40ff
amaar <amaar9008@gmail.com>
2023-02-17 07:06:59 +0500

    nothing

--- main.dart
+++ main.dart
@@ -16,17 +16,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-    @override
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: Text('mmeals'),
       ),
       body: Center(
         child: Text('Navigation Time!'),
